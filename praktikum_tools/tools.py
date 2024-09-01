2f53646f9e522fff6ea9f9157a5df71d9786c11e
Eva <evatill@uni-bonn.de>
2024-09-01 19:11:41 +0200

    fixed issue with overdraw

--- praktikum_tools/tools.py
+++ praktikum_tools/tools.py
@@ -209,7 +209,7 @@ class Plot:
         return self
     def func(self, f, x, param = (), overdraw = True, **kwargs):
         x = smooth_range(x, overdraw = overdraw)
-        self.line(x, f(x, *param), *args, **kwargs)
+        self.line(x, f(x, *param), **kwargs)
         return self
     def area(self, value, stop = None, color = 'green', alpha = 0.5, autoscale = True, **kwargs):
         with self.autoscale(autoscale):
