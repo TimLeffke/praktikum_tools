935ccf6e30225c015f196dcd34bfdc23f96922c9
Eva <evatill@uni-bonn.de>
2024-09-01 19:04:54 +0200

    fixed typos

--- examples/example_gauss.py
+++ examples/example_gauss.py
@@ -4,8 +4,8 @@ import praktikum_tools as tl
 x = np.linspace(-1, 3, 100)
 x = tl.ErrValue(x, 0.1)
 
-y = gauss(x, {'A': [4], 'sigma': [1.5], 'mu': [1]})
+y = tl.gauss(x, {'A': [4], 'sigma': [1.5], 'mu': [1], 'background': []})
 
-p = Plot()
+p = tl.Plot()
 p.add(x, y)
 p.show()
