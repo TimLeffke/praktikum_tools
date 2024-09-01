bcd9c582023e5f9a0ef3b68f651f603d17279198
Eva <evatill@uni-bonn.de>
2024-09-01 17:28:54 +0200

    added examples

---
+++ examples/example_custom_f_fit.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tools as tl
+
+def sin(x, A, omega, phase):
+    return A * tl.sin(x * omega + phase)
+
+
+x = np.linspace(-1, 1, 10)
+x = tl.ErrValue(x, 0.01)
+
+y = sin(x, 2.5, 1.2, 0.3) + np.random.normal(loc = 0, scale = 0.1, size = len(x))
+y.error = 0.1
+
+A, omega, phase = tl.fit_func(sin, x, y)
+
+p = tl.Plot(xlabel = "x", ylabel = r"$A\sin(\varphi_0 + \omega x)$")
+p.add(x, y)
+p.func(sin, (x[0] - 1, x[-1] + 1), [A, omega, phase], autoscale = False)
+p.show()
---
+++ examples/example_plot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tools as tl
+
+# Create x values
+x = np.linspace(0, 3*np.pi, 100)
+
+# x values should have an error of 0.05
+x = tl.ErrValue(x, 0.05)
+
+y = tl.sin(x)
+
+
+# Create Plot object
+p = tl.Plot(title = "Best Plot", xlabel = "x", ylabel = "y")
+# Add data
+p.add(x, y)
+# Show plot)
+p.show()
