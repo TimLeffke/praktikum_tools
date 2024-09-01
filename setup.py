de1efb75240869ea5bd5cffb64b792f0d9cbb6c7
Eva <evatill@uni-bonn.de>
2024-09-01 18:32:55 +0200

    Reworked structure to be installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='praktikum_tools',
+    version='0.1',
+    description='A library to work with simple experiment analysis',
+    url='https://github.com/TimLeffke/praktikum_tools.git',
+    author='Tim Leffke',
+    packages=['praktikum_tools'],
+    install_requires=['numpy', 'matplotlib', 'scipy']
+)
